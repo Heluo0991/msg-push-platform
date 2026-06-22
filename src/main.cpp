16b0009f5a7c1a3a36a31a0c04da05159cf00632
Heluo0991 <2971411759@qq.com>
2026-06-22 21:53:06 +0800

    feat: MPSCqueue for process rawmessage

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include "reactor.h"
-#include"lockfree_queue.h"
-int main()
+#include "lockfree_queue.h"
+#include "rawmessage.h"
+#include"mpmc_queue.h"
+
+
+int main(int argc, char *argv[])
 {
-    Reactor reactor(8080);
-    reactor.run();
-    return 0;
+    std::vector<std::unique_ptr<LockFreeQueue<RawMessage, 1024>>> Workers;
+    int workernum = 4;//无参数时默认SPSC队列数量
+    if (argc == 3 && std::string(argv[1]) == "-w")
+    {                                   // 传了三个值，包括"-w"和线程数argv[2]
+        workernum = std::atoi(argv[2]); // 有参数就改workernum
+    }
+
+    for (int i = 0; i < workernum; i++)
+    {
+        Workers.emplace_back(std::make_unique<LockFreeQueue<RawMessage, 1024>>());
+        // std::make_unique有noexcept，vector扩容不会退化成拷贝构造(保证可回退)
+        // 临时匿名右值unique_ptr直接把他申请的堆内存交给workers的实例管理
+    }
 }
--- src/rawmessage.h
+++ src/rawmessage.h
@@ -2,7 +2,7 @@
 #define RAWMESSAGE
 #include<memory>
 #include"connection.h"
-struct RawMessgae{
+struct RawMessage{
     std::weak_ptr<Connection> wp_;//指向Connection的弱指针
     std::string raw;//生字符串本身
 };
